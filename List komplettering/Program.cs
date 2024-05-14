3340289966cce28423ae62f8ff0849206b7b64ba
Theo Lee <theo.leehao@elev.ga.ntig.se>
2024-05-14 10:44:35 +0200

    lista

--- List komplettering/Program.cs
+++ List komplettering/Program.cs
@@ -3,8 +3,33 @@
 System.Console.WriteLine("You have a list with 3 names would you like to add names or would you like to print the list of names?");
 System.Console.WriteLine("To add names type add and to print the list type print");
 String Answer = Console.ReadLine();
+
+while (true)
+{
+    if (Answer == "Add" || Answer == "add")
+    {
+        break;
+    }
+    else if (Answer == "Print" || Answer == "print")
+    {
+        break;
+    }
+    else
+    {
+        System.Console.WriteLine("Type a valid input");
+        Answer = Console.ReadLine();
+    }
+
+}
+
 if (Answer == "Add" || Answer == "add")
+{
+    names.Add(Console.ReadLine());
+}
 
-
+foreach (String name in names)
+{
+    System.Console.WriteLine(name);
+}
 
 Console.ReadLine();
