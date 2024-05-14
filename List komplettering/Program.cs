94f7eca5ac498e7191364f36e01c91f85d1c47b6
Theo Lee <theo.leehao@elev.ga.ntig.se>
2024-05-14 10:27:55 +0200

    first commit

---
+++ List komplettering/Program.cs
@@ -0,0 +1,10 @@
+﻿List <string> names = new List<string> {"Anders", "Klas", "Filip"};
+
+System.Console.WriteLine("You have a list with 3 names would you like to add names or would you like to print the list of names?");
+System.Console.WriteLine("To add names type add and to print the list type print");
+String Answer = Console.ReadLine();
+if (Answer == "Add" || Answer == "add")
+
+
+
+Console.ReadLine();
